756d983b544ec373f08ac99f10daf41273ddb772
kazinayeem <mohammadalinayeem360@gmail.com>
2026-01-09 21:39:27 +0600

    Update deployment script to set correct project directory for deployment

--- backend/ecosystem.config.cjs
+++ backend/ecosystem.config.cjs
@@ -1,11 +1,12 @@
 module.exports = {
   apps: [
     {
-      name: "oriyet-backend-dev",
+      name: "oriyet-backend",
       cwd: "/root/oriyet/backend",
       script: "npm",
       args: "run dev",
-      watch: true,
+      watch: false,
+      autorestart: true,
       env: {
         NODE_ENV: "development"
       }
